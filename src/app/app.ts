0c6b9f274a668bd689cb99b2ac92c5d9bd54a972
juanDev111020 <jdgnarvaez1@gmail.com>
2026-03-07 10:22:38 -0500

    Consumir PokeApi

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('pokemon-app');
+export class App implements OnInit {
+
+  pokemons: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+
+    this.http.get<any>('https://pokeapi.co/api/v2/pokemon')
+      .subscribe(data => {
+
+        this.pokemons = data.results.map((pokemon: any, index: number) => {
+
+          return {
+            name: pokemon.name,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+          };
+
+        });
+
+      });
+
+  }
+
 }
